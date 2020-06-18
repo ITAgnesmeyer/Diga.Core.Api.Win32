6af2cbbfcd259eeb7d992902e383f2a499888621
DIGA <guido.agnesmeyer@itagnesmeyer.de>
2020-06-18 16:00:48 +0200

    BrowseForFolder

--- Diga.Core.Api.Win32/User32.cs
+++ Diga.Core.Api.Win32/User32.cs
@@ -40,6 +40,8 @@ namespace Diga.Core.Api.Win32
         private static extern bool AppendMenu(IntPtr hMenu, MenuFlags uFlags, uint uIdNewItem, string lpNewItem);
 
 
+        [DllImport(USER32, EntryPoint = "CreateWindowEx", CharSet = CHARSET, SetLastError = true)]
+        public static extern IntPtr CreateWindowEx(uint dwExStyle, [In] string lpClassName, [In] string lpWindowName, uint dwStyle, int X, int Y, int nWidth, int nHeight, [In] IntPtr hWndParent, [In] IntPtr hMenu, [In] IntPtr hInstance, [In] IntPtr lpParam);
 
 
         [DllImport(USER32, EntryPoint = "CreateWindowEx", SetLastError = true, CharSet = CHARSET)]
@@ -61,7 +63,7 @@ namespace Diga.Core.Api.Win32
 
 
 
-        [DllImport(USER32, EntryPoint = "RegisterClassEx", CharSet = CHARSET)]
+        [DllImport(USER32, EntryPoint = "RegisterClassEx",SetLastError =true, CharSet = CHARSET)]
         public static extern ushort RegisterClassEx([In()] ref WndclassEx param0);
 
 
@@ -239,6 +241,8 @@ namespace Diga.Core.Api.Win32
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool SystemParametersInfo(uint uiAction, int uiParam, ref LogFont logFont, int fWinIni);
 
+        [DllImportAttribute(USER32, EntryPoint = "GetDesktopWindow")]
+        public static extern IntPtr GetDesktopWindow();
 
         public static bool GetDefaultLogFont(out LogFont logFont)
         {
@@ -278,7 +282,7 @@ namespace Diga.Core.Api.Win32
         }
 
 
-        [DllImport(USER32, CharSet = CharSet.Auto, ExactSpelling = true)]
+        [DllImport(USER32, CharSet = CharSet.Auto)]
         public static extern IntPtr GetActiveWindow();
 
 
@@ -587,10 +591,80 @@ namespace Diga.Core.Api.Win32
         ///lprcUpdate: RECT*
         ///hrgnUpdate: HRGN->HRGN__*
         ///flags: UINT->unsigned int
-        [DllImport("user32.dll", EntryPoint="RedrawWindow")]
+        [DllImport(USER32, EntryPoint="RedrawWindow")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern  bool RedrawWindow([In] IntPtr hWnd, [In] IntPtr lprcUpdate, [In] IntPtr hrgnUpdate, uint flags) ;
 
 
+    }
+
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public delegate int BrowseCallBackProc(IntPtr hwnd, uint msg, IntPtr lp, IntPtr wp);
+
+    [StructLayoutAttribute(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct BROWSEINFO
+    {
+        public IntPtr hwndOwner;
+        public IntPtr pidlRoot;
+        public string pszDisplayName;
+        public string lpszTitle;
+        public uint ulFlags;
+        public BrowseCallBackProc lpfn;
+        public IntPtr lParam;
+        public int iImage;
+    }
+    public static class Shell32
+    {
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SHBrowseForFolder(ref BROWSEINFO lpbi);
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        public static extern bool SHGetPathFromIDList(IntPtr pidl, StringBuilder pszPath);
+
+
+    }
+    public static class BrowseForFolderConst
+    {
+        // Constants for sending and receiving messages in BrowseCallBackProc
+        
+        public const uint BFFM_INITIALIZED = 1;
+        public const uint BFFM_SELCHANGED = 2;
+        public const uint BFFM_VALIDATEFAILEDA = 3;
+        public const uint BFFM_VALIDATEFAILEDW = 4;
+        public const uint BFFM_IUNKNOWN = 5; // provides IUnknown to client. lParam: IUnknown*
+        public const uint BFFM_SETSTATUSTEXTA = WindowsMessages.WM_USER + 100;
+        public const uint BFFM_ENABLEOK = WindowsMessages.WM_USER + 101;
+        public const uint BFFM_SETSELECTIONA = WindowsMessages.WM_USER + 102;
+        public const uint BFFM_SETSELECTIONW = WindowsMessages.WM_USER + 103;
+        public const uint BFFM_SETSTATUSTEXTW = WindowsMessages.WM_USER + 104;
+        public const uint BFFM_SETOKTEXT = WindowsMessages.WM_USER + 105; // Unicode only
+        public const uint BFFM_SETEXPANDED = WindowsMessages.WM_USER + 106; // Unicode only
+
+        // Browsing for directory.
+        public const uint BIF_RETURNONLYFSDIRS = 0x0001;  // For finding a folder to start document searching
+        public const uint BIF_DONTGOBELOWDOMAIN = 0x0002;  // For starting the Find Computer
+        public const uint BIF_STATUSTEXT = 0x0004;  // Top of the dialog has 2 lines of text for BROWSEINFO.lpszTitle and one line if
+        // this flag is set.  Passing the message BFFM_SETSTATUSTEXTA to the hwnd can set the
+        // rest of the text.  This is not used with BIF_USENEWUI and BROWSEINFO.lpszTitle gets
+        // all three lines of text.
+        public const uint BIF_RETURNFSANCESTORS = 0x0008;
+        public const uint BIF_EDITBOX = 0x0010;   // Add an editbox to the dialog
+        public const uint BIF_VALIDATE = 0x0020;   // insist on valid result (or CANCEL)
+
+        public const uint BIF_NEWDIALOGSTYLE = 0x0040;   // Use the new dialog layout with the ability to resize
+        // Caller needs to call OleInitialize() before using this API
+        public const uint BIF_USENEWUI = 0x0040 + 0x0010; //(BIF_NEWDIALOGSTYLE | BIF_EDITBOX);
+
+        public const uint BIF_BROWSEINCLUDEURLS = 0x0080;   // Allow URLs to be displayed or entered. (Requires BIF_USENEWUI)
+        public const uint BIF_UAHINT = 0x0100;   // Add a UA hint to the dialog, in place of the edit box. May not be combined with BIF_EDITBOX
+        public const uint BIF_NONEWFOLDERBUTTON = 0x0200;   // Do not add the "New Folder" button to the dialog.  Only applicable with BIF_NEWDIALOGSTYLE.
+        public const uint BIF_NOTRANSLATETARGETS = 0x0400;  // don't traverse target as shortcut
+
+        public const uint BIF_BROWSEFORCOMPUTER = 0x1000;  // Browsing for Computers.
+        public const uint BIF_BROWSEFORPRINTER = 0x2000;// Browsing for Printers
+        public const uint BIF_BROWSEINCLUDEFILES = 0x4000; // Browsing for Everything
+        public const uint BIF_SHAREABLE = 0x8000;  // sharable resources displayed (remote shares, requires BIF_USENEWUI)
     }
 }
---
+++ SurfaceTest/Form1.cs
@@ -0,0 +1,113 @@
+﻿using Diga.Core.Api.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SurfaceTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TestOpenFolder of = new TestOpenFolder(User32.GetActiveWindow());
+            var retValue = of.ShowBrowseForFolder();
+            if(retValue == DialogResult.OK)
+            {
+                this.Text = of.Path;
+            }
+        }
+    }
+
+    class TestOpenFolder
+    {
+        private string _InitalPath;
+        private IntPtr _HWnd;
+        public string Path { get; set; }
+        public TestOpenFolder(IntPtr parentHwned, string initPath="")
+        {
+            this._HWnd = parentHwned;
+            this._InitalPath = initPath;
+        }
+        public DialogResult ShowBrowseForFolder()
+        {
+            DialogResult resultValue = DialogResult.Cancel;
+            InitCommonControlsEx cc = new InitCommonControlsEx(CommonControls.ICC_UNDEFINED);
+            ComCtl32.InitCommonControlsEx(ref cc);
+            int cs = Marshal.SystemDefaultCharSize;
+            if (cs == 2)
+                Debug.Print("unicode");
+            else
+                Debug.Print("Ansi");
+
+
+            BROWSEINFO bf = new BROWSEINFO();
+            bf.hwndOwner = this._HWnd;
+            bf.pidlRoot = IntPtr.Zero;
+            bf.lpszTitle = "TEST";
+            bf.ulFlags = BrowseForFolderConst.BIF_NEWDIALOGSTYLE | BrowseForFolderConst.BIF_SHAREABLE;
+            bf.lpfn = new BrowseCallBackProc(OnBffCallByck);
+            bf.lParam = IntPtr.Zero;
+            bf.iImage = 0;
+            IntPtr pidl = IntPtr.Zero;
+            try
+            {
+                StringBuilder sb = new StringBuilder(256);
+                pidl = Shell32.SHBrowseForFolder(ref bf);
+                
+                bool retVal = Shell32.SHGetPathFromIDList(pidl, sb);
+                if (retVal)
+                {
+                    Debug.Print(sb.ToString());
+                    this.Path = sb.ToString();
+                    resultValue = DialogResult.OK;
+                }
+                else
+                {
+                    resultValue = DialogResult.Cancel;
+                }
+            }
+            finally
+            {
+                if (pidl != IntPtr.Zero)
+                    Marshal.FreeCoTaskMem(pidl);
+
+            }
+            return resultValue;
+        }
+
+        private int OnBffCallByck(IntPtr hwnd, uint msg, IntPtr lp, IntPtr wp)
+        {
+            switch (msg)
+            {
+                case BrowseForFolderConst.BFFM_INITIALIZED:
+                    User32.SendMessage(hwnd, (int)BrowseForFolderConst.BFFM_SETSELECTIONW, 1, _InitalPath);
+                    break;
+                case BrowseForFolderConst.BFFM_SELCHANGED:
+                    StringBuilder sb = new StringBuilder(256);
+                    if (Shell32.SHGetPathFromIDList(lp, sb))
+                    {
+                        User32.SendMessage(hwnd, (int)BrowseForFolderConst.BFFM_SETSTATUSTEXTW, 0, sb);
+                    }
+                    break;
+            }
+            return 0;
+        }
+    }
+
+
+
+
+}
