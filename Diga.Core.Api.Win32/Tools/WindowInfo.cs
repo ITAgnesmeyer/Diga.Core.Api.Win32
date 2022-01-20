77e53b6bb6f576cfee4a5df8bc9d2e995f165f4a
DCSItoDIGA <guido.agnesmeyer@itagnesmeyer.de>
2022-01-20 13:35:17 +0100

    Change enumWindow calling

--- Diga.Core.Api.Win32/Tools/WindowInfo.cs
+++ Diga.Core.Api.Win32/Tools/WindowInfo.cs
@@ -6,22 +6,27 @@ namespace Diga.Core.Api.Win32.Tools
     public static class WindowInfo
     {
       
-
+        private static Dictionary<IntPtr, string> _windows = new Dictionary<IntPtr, string>();
+        private static IntPtr shellWindow;
         public static IDictionary<IntPtr, string> GetTopLevelWindows()
         {
+            _windows.Clear();
 
-            IntPtr shellWindow = User32.GetShellWindow();
-            Dictionary<IntPtr, string> windows = new Dictionary<IntPtr, string>();
+            shellWindow = User32.GetShellWindow();
+            
+            WndEumProc proc = EnumProc; 
+            User32.EnumWindows(proc,IntPtr.Zero);
+            return _windows;
+        }
 
-            User32.EnumWindows((hWnd, lParam) =>
-            {
-                if (hWnd == shellWindow) return (ApiBool)true;
+        private static int EnumProc(IntPtr hwnd, IntPtr lparam)
+        {
+            
+            if (hwnd == shellWindow) return (ApiBool)true;
                 
-                string className = User32.GetClassName(hWnd);
-                windows.Add(hWnd, className);
-                return (ApiBool)true;
-            },0);
-            return windows;
+            string className = User32.GetClassName(hwnd);
+            _windows.Add(hwnd, className);
+            return (ApiBool)true;
         }
     }
 }
